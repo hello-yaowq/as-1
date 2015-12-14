53146b18dd6a1361ebf4d61240cc5a987ebf128c
parai.wang <parai@foxmail.com>
2015-12-14 22:09:27 +0800

    add erase function

--- com/as.infrastructure/arch/stm32f1/mcal/Flash.c
+++ com/as.infrastructure/arch/stm32f1/mcal/Flash.c
@@ -14,18 +14,21 @@
  */
 
 /* ============================ [ INCLUDES  ] ====================================================== */
+#include "Std_Types.h"
 #include "Flash.h"
 #include "stm32f10x_flash.h"
 /* ============================ [ MACROS    ] ====================================================== */
+#define FLASH_ERASE_SIZE  512 	/* TODO */
+#define FLASH_IS_ERASE_ADDRESS_ALIGNED(a)  ( 0 == ((FLASH_ERASE_SIZE-1)&(a)) )
 
+#define FLASH_WRITE_SIZE  2 		/* TODO */
+#define FLASH_IS_WRITE_ADDRESS_ALIGNED(a)  ( 0 == ((FLASH_WRITE_SIZE-1)&(a)) )
 /* ============================ [ TYPES     ] ====================================================== */
 /* ============================ [ DECLARES  ] ====================================================== */
 /* ============================ [ DATAS     ] ====================================================== */
 tFlashHeader FlashHeader =
 {
-	.Info.W.MCU     = 1,
-	.Info.W.mask    = 2,
-	.Info.W.version = 169,
+	.Info.w    = 0x01004512,
 	.Init      = FlashInit,
 	.Deinit    = FlashDeinit,
 	.Erase     = FlashErase,
@@ -39,7 +42,11 @@ void FlashInit(tFlashParam* FlashParam)
 		 (FLASH_DRIVER_VERSION_MINOR == FlashParam->minornumber) ||
 		 (FLASH_DRIVER_VERSION_MAJOR == FlashParam->majornumber) )
 	{
-		/*  TODO: init Flash Controllor */
+		/* Unlock the Flash Program Erase controller */
+		FLASH_Unlock();
+		/* Clear All pending flags */
+		FLASH_ClearFlag(FLASH_FLAG_BSY | FLASH_FLAG_EOP | FLASH_FLAG_PGERR | FLASH_FLAG_WRPRTERR);
+
 		FlashParam->errorcode = kFlashOk;
 	}
 	else
@@ -50,16 +57,86 @@ void FlashInit(tFlashParam* FlashParam)
 
 void FlashDeinit(tFlashParam* FlashParam)
 {
-	/*  TODO: Deinit Flash Controllor */
+	FLASH_Lock();
 	FlashParam->errorcode = kFlashOk;
 }
 
 void FlashErase(tFlashParam* FlashParam)
 {
+	tAddress address;
+	tAddress length;
+	if ( (FLASH_DRIVER_VERSION_PATCH == FlashParam->patchlevel) ||
+		 (FLASH_DRIVER_VERSION_MINOR == FlashParam->minornumber) ||
+		 (FLASH_DRIVER_VERSION_MAJOR == FlashParam->majornumber) )
+	{
+		length = FlashParam->length;
+		address = FlashParam->address;
+		if ( (FALSE == FLASH_IS_ERASE_ADDRESS_ALIGNED(address)) ||
+			 (FALSE == IS_FLASH_ADDRESS(address)) )
+		{
+			FlashParam->errorcode = kFlashInvalidAddress;
+		}
+		else if( (FALSE == IS_FLASH_ADDRESS(address+length)) ||
+				 (FALSE == FLASH_IS_ERASE_ADDRESS_ALIGNED(length)) )
+		{
+			FlashParam->errorcode = kFlashInvalidSize;
+		}
+		else
+		{
+			/* Erase the sector... */
+			while(length > 0)
+			{
+				volatile FLASH_Status status = FLASH_BUSY;
+				while(status != FLASH_COMPLETE){
+					status = FLASH_ErasePage((uint32_t)address);
+				}
 
+				length  -= FLASH_ERASE_SIZE;
+				address += FLASH_ERASE_SIZE;
+			}
+			FlashParam->errorcode = kFlashOk;
+		}
+	}
+	else
+	{
+		FlashParam->errorcode = kFlashFailed;
+	}
 }
 
 void FlashWrite(tFlashParam* FlashParam)
 {
+	tAddress address;
+	tAddress length;
+	tData*    data;
+	if ( (FLASH_DRIVER_VERSION_PATCH == FlashParam->patchlevel) ||
+		 (FLASH_DRIVER_VERSION_MINOR == FlashParam->minornumber) ||
+		 (FLASH_DRIVER_VERSION_MAJOR == FlashParam->majornumber) )
+	{
+		length = FlashParam->length;
+		address = FlashParam->address;
+		data = FlashParam->data;
+		if ( (FALSE == FLASH_IS_ERASE_ADDRESS_ALIGNED(address)) ||
+			 (FALSE == IS_FLASH_ADDRESS(address)) )
+		{
+			FlashParam->errorcode = kFlashInvalidAddress;
+		}
+		else if( (FALSE == IS_FLASH_ADDRESS(address+length)) ||
+				 (FALSE == FLASH_IS_ERASE_ADDRESS_ALIGNED(length)) )
+		{
+			FlashParam->errorcode = kFlashInvalidSize;
+		}
+		else if( NULL == data )
+		{
+			FlashParam->errorcode = kFlashInvalidData;
+		}
+		else
+		{
+			FlashParam->errorcode = kFlashOk;
+		}
 
+	}
+	else
+	{
+		FlashParam->errorcode = kFlashFailed;
+	}
 }
