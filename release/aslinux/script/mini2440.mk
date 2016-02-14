4df351a5a5b051752e06640fe0fdcf13812033b6
parai <parai@foxmail.com>
2016-02-14 18:25:32 +0800

    add dnw2 tool for mini2440

--- release/aslinux/script/mini2440.mk
+++ release/aslinux/script/mini2440.mk
@@ -14,7 +14,7 @@ rootfs = $(out)/rootfs
 download = $(CURDIR)/download
 
 # first default make
-all:$(rootfs) asmini2440-kernel asmini2440-uboot asmini2440-qemu nand
+all:$(rootfs) asmini2440-kernel asmini2440-uboot asmini2440-qemu busybox nand
 	@echo "  >> build mini2440 done <<"
 
 $(rootfs):
@@ -74,17 +74,25 @@ asuboot:u-boot
 	@(cd u-boot;cp -v u-boot $(rootfs))
 
 extract-busybox:
-	@tar -zxvf $(download)/busybox-1.2.0.tgz -C $(CURDIR)
-	@mv busybox-1.2.0 busybox
+	@bzip2 -dvk $(download)/busybox-1.24.0.tar.bz2
+	@tar -xvf $(download)/busybox-1.24.0.tar -C $(CURDIR)
+	@rm $(download)/busybox-1.24.0.tar
+	@mv busybox-1.24.0 busybox
+
+$(download)/busybox-1.24.0.tar.bz2:
+	@(cd $(download);wget http://busybox.net/downloads/busybox-1.24.0.tar.bz2)
+	@make extract-busybox
 
 busybox-menuconfig:
 	@(cd busybox;make menuconfig)
 
 busybox/.config:
+	@(cd busybox;make menuconfig)
 
-asbusybox: busybox/.config
+asbusybox:$(download)/busybox-1.24.0.tar.bz2 busybox/.config
 	@(cd busybox;make all CROSS_COMPILE=arm-linux-gnueabi-)
 	@(cd busybox;make install CONFIG_PREFIX=$(rootfs))
+
 
 extract-glibc:
 	@bzip2 -dvk $(download)/glibc-2.22.tar.bz2
@@ -123,8 +131,10 @@ sdcard:$(out)/sdcard.ext3 asrootfs
 		sudo umount tmp;	\
 		rm tmp -fr)
 
-nand:$(out)/rootfs.jffs2
+nand:
 #	@cp -v flashimg/uboot.part $(out)
+	@mkdir -p $(rootfs)/dev $(rootfs)/mnt $(rootfs)/tmp
+	@(cd $(rootfs)/dev;sudo mknod -m 660 null c 1 3; sudo mknod -m 660 console c 5 1)
 	@(cd $(out); mkfs.jffs2 -r $(rootfs) -o rootfs.jffs2 -e 16KiB --pad=0x3aa0000 -s 0x200 -n －b)
 	@(cd $(out);	\
 		flashimg -s 64M -t nand -f nand.bin -p uboot.part -w boot,u-boot.bin -w kernel,uImage -w root,rootfs.jffs2 -z 512)
@@ -133,7 +143,7 @@ run-qemu-mini2440:
 	@opt/bin/qemu-system-arm -M mini2440 -serial stdio -mtdblock out/nand.bin -usbdevice mouse
 # for the first start of qemu, need do the below command
 # MINI2440 # nboot kernel
-# MINI2440 # setenv bootargs root=/dev/mtdblock3 rootfstype=jffs2 console=ttySAC0,115200 mini2440=3tb
+# MINI2440 # setenv bootargs root=/dev/mtdblock3 rootfstype=jffs2 console=ttySAC0,115200 mini2440=3tb init=/linuxrc
 # MINI2440 # saveenv
 # MINI2440 # bootm
 example:
