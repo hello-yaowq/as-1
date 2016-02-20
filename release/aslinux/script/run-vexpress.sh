27b895ea732762c693b31c5c309113366f6b8639
parai <parai@foxmail.com>
2016-02-20 11:20:54 +0800

    try to use with getopt

--- release/aslinux/script/run-vexpress.sh
+++ release/aslinux/script/run-vexpress.sh
@@ -1,32 +1,34 @@
 #! /bin/sh
 
-option=0
+# show usage
+show_usage="args: [-g <yes/no>, -d <yes/no>]	\
+                  [--gui <yes/no>, --debug <yes/no>]"
+opt_gui="no"
+opt_debug="no"
 
-cd out
-
-if [ $# -eq 1 ] ; then 
-	if [ $1 = 'd' ]; then
-		option=1
-	elif [ $1 = 'n' ]; then
-		option=2
-	fi
-fi
-if [ $option -eq 1 ] ; then
-	echo "  >> DEBUG by gdb"
-	echo "     arm-linux-gnueabihf-gdb"
-	echo "      file vmlinux"
-	echo "      target remote:1234"
-	echo "      b start_kernel"
-    qemu-system-arm -kernel rootfs/zImage -dtb rootfs/vexpress-v2p-ca9.dtb -M vexpress-a9 \
-    	-append "root=/dev/mmcblk0 console=ttyAMA0 console=tty0" -sd sdcard.ext3 -serial stdio -s -S
-elif [ $option -eq 2 ] ; then
-	qemu-system-arm -M vexpress-a9 -m 512M -kernel rootfs/zImage -dtb rootfs/vexpress-v2p-ca9.dtb \
-		-nographic -append "init=/linuxrc root=/dev/mmcblk0 rw console=ttyAMA0"	\
-		-sd sdcard.ext3 -smp 4 -m 512M
+GETOPT_ARGS=`getopt -o g:d: -al gui:,debug: -- "$@"`
+eval set -- "$GETOPT_ARGS"
+# get parameter
+while [ -n "$1" ]
+do
+    case "$1" in
+        -g|--gui) opt_gui=$2; shift 2;;
+        -d|--debug) opt_debug=$2; shift 2;;
+        --) break ;;
+        *) echo $1,$2,$show_usage; break ;;
+    esac
+done
+echo "#! /bin/sh" > tmp_qemu_run.sh
+echo "cd out" >> tmp_qemu_run.sh
+echo "qemu-system-arm -kernel rootfs/zImage -dtb rootfs/vexpress-v2p-ca9.dtb -M vexpress-a9 \\" >> tmp_qemu_run.sh
+if [ $opt_gui = "no" ]; then
+	echo "  -nographic -append \"init=/linuxrc root=/dev/mmcblk0 rw console=ttyAMA0\"\\" >> tmp_qemu_run.sh
+	echo "  -sd sdcard.ext3 -smp 4 -m 512M" >> tmp_qemu_run.sh
 else
-    qemu-system-arm -M vexpress-a9 -kernel rootfs/zImage -dtb rootfs/vexpress-v2p-ca9.dtb \
-    	-append "init=/linuxrc root=/dev/mmcblk0 rw console=ttyAMA0 console=tty0" \
-    	-sd sdcard.ext3 -serial stdio -net nic -net user -smp 4 -m 512M
-
-#-usb -usbdevice touchscreen
+	echo "  -append \"init=/linuxrc root=/dev/mmcblk0 rw console=ttyAMA0 console=tty0\" \\" >> tmp_qemu_run.sh
+	echo "  -sd sdcard.ext3 -serial stdio -net nic -net user -smp 4 -m 512M" >> tmp_qemu_run.sh
 fi
+
+chmod +x tmp_qemu_run.sh
+./tmp_qemu_run.sh
+exit
