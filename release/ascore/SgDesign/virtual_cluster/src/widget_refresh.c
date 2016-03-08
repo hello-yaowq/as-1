a115b11029fc6fdea686531e7bfc1d72a45991f3
parai <parai@foxmail.com>
2016-03-08 23:01:02 +0800

    add pointer support

--- release/ascore/SgDesign/virtual_cluster/src/widget_refresh.c
+++ release/ascore/SgDesign/virtual_cluster/src/widget_refresh.c
@@ -1,21 +1,36 @@
 
 #include "Sg.h"
 #include <time.h>
-void* RefreshPointerHour(SgWidget* w)
+void* RefreshClusterTachoPointer(SgWidget* w)
 {
 	time_t t = time(0);
 	struct tm* lt = localtime(&t);
 
-	if(lt->tm_hour < 3)
+	if(lt->tm_sec < 15)
 	{
-		w->d = 360 - (3-lt->tm_hour)*30;
+		w->d = 360 - (15 -lt->tm_sec)*6;
 	}
 	else
 	{
-		w->d = (lt->tm_hour-3)*30;
+		w->d = (lt->tm_sec-15)*6;
 	}
 
-	w->d += lt->tm_min*30/60;
+	return 0;
+}
+
+void* RefreshClusterSpeedPointer(SgWidget* w)
+{
+	time_t t = time(0);
+	struct tm* lt = localtime(&t);
+
+	if(lt->tm_sec < 15)
+	{
+		w->d = 360 - (15 -lt->tm_sec)*6;
+	}
+	else
+	{
+		w->d = (lt->tm_sec-15)*6;
+	}
 
 	return 0;
 }
