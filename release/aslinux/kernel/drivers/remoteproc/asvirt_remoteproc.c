1bb1e9118b28afbec4330e2768a7418a74811827
parai <parai@foxmail.com>
2016-07-09 11:31:39 +0800

    aslinux:virtio study start

---
+++ release/aslinux/kernel/drivers/remoteproc/asvirt_remoteproc.c
@@ -0,0 +1,224 @@
+/**
+ * AS - the open source Automotive Software on https://github.com/parai
+ *
+ * Copyright (C) 2016  AS <parai@foxmail.com>
+ *
+ * This source code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation; See <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+/* ============================ [ INCLUDES  ] ====================================================== */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/err.h>
+#include <linux/platform_device.h>
+#include <linux/dma-mapping.h>
+#include <linux/remoteproc.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_device.h>
+#include <linux/of_platform.h>
+#include "remoteproc_internal.h"
+
+/* ============================ [ MACROS    ] ====================================================== */
+
+/* ============================ [ TYPES     ] ====================================================== */
+
+/*
+ * struct asvirt_rproc_pdata - asvirt remoteproc's platform data
+ * @name: the remoteproc's name
+ * @oh_name: asvirt hwmod device
+ * @oh_name_opt: optional, secondary asvirt hwmod device
+ * @firmware: name of firmware file to load
+ * @mbox_name: name of asvirt mailbox device to use with this rproc
+ * @ops: start/stop rproc handlers
+ * @device_enable: asvirt-specific handler for enabling a device
+ * @device_shutdown: asvirt-specific handler for shutting down a device
+ * @set_bootaddr: asvirt-specific handler for setting the rproc boot address
+ */
+struct asvirt_rproc_pdata {
+	const char *name;
+	const char *oh_name;
+	const char *oh_name_opt;
+	const char *firmware;
+	const char *mbox_name;
+	const struct rproc_ops *ops;
+	int (*device_enable) (struct platform_device *pdev);
+	int (*device_shutdown) (struct platform_device *pdev);
+	void(*set_bootaddr)(u32);
+};
+
+/**
+ * struct asvirt_rproc - asvirt remote processor state
+ * @rproc: rproc handle
+ */
+struct asvirt_rproc {
+	struct rproc *rproc;
+};
+/* ============================ [ DECLARES  ] ====================================================== */
+static int asvirt_rproc_probe(struct platform_device *pdev);
+static int asvirt_rproc_remove(struct platform_device *pdev);
+static void asvirt_rproc_kick(struct rproc *rproc, int vqid);
+static int asvirt_rproc_stop(struct rproc *rproc);
+static int asvirt_rproc_start(struct rproc *rproc);
+/* ============================ [ DATAS     ] ====================================================== */
+static struct rproc_ops asvirt_rproc_ops = {
+	.start		= asvirt_rproc_start,
+	.stop		= asvirt_rproc_stop,
+	.kick		= asvirt_rproc_kick,
+};
+
+
+static struct of_device_id asvirt_rproc_of_match[] = {
+	{ .compatible = "as,virtual-rproc" },
+};
+
+static struct platform_driver asvirt_rproc_driver = {
+	.probe = asvirt_rproc_probe,
+	.remove = asvirt_rproc_remove,
+	.driver = {
+		.name = "asvirt-rproc",
+		.owner = THIS_MODULE,
+		.of_match_table = asvirt_rproc_of_match,
+	},
+};
+/* ============================ [ LOCALS    ] ====================================================== */
+
+/* kick a virtqueue */
+static void asvirt_rproc_kick(struct rproc *rproc, int vqid)
+{
+	struct asvirt_rproc *oproc = rproc->priv;
+	struct device *dev = rproc->dev.parent;
+	int ret;
+
+	/* send the index of the triggered virtqueue in the mailbox payload */
+	ret = 0;
+	if (ret)
+		dev_err(dev, "asvirt_mbox_msg_send failed: %d\n", ret);
+}
+
+/*
+ * Power up the remote processor.
+ *
+ * This function will be invoked only after the firmware for this rproc
+ * was loaded, parsed successfully, and all of its resource requirements
+ * were met.
+ */
+static int asvirt_rproc_start(struct rproc *rproc)
+{
+	struct asvirt_rproc *oproc = rproc->priv;
+	struct device *dev = rproc->dev.parent;
+	struct platform_device *pdev = to_platform_device(dev);
+	struct asvirt_rproc_pdata *pdata = pdev->dev.platform_data;
+	int ret;
+
+	if (pdata->set_bootaddr)
+		pdata->set_bootaddr(rproc->bootaddr);
+
+	/*
+	 * Ping the remote processor. this is only for sanity-sake;
+	 * there is no functional effect whatsoever.
+	 *
+	 * Note that the reply will _not_ arrive immediately: this message
+	 * will wait in the mailbox fifo until the remote processor is booted.
+	 */
+	ret = 0;
+	if (ret) {
+		dev_err(dev, "asvirt_mbox_get failed: %d\n", ret);
+		goto put_mbox;
+	}
+
+	ret = pdata->device_enable(pdev);
+	if (ret) {
+		dev_err(dev, "asvirt_device_enable failed: %d\n", ret);
+		goto put_mbox;
+	}
+
+	return 0;
+
+put_mbox:
+
+	return ret;
+}
+
+/* power off the remote processor */
+static int asvirt_rproc_stop(struct rproc *rproc)
+{
+	struct device *dev = rproc->dev.parent;
+	struct platform_device *pdev = to_platform_device(dev);
+	struct asvirt_rproc_pdata *pdata = pdev->dev.platform_data;
+	struct asvirt_rproc *oproc = rproc->priv;
+	int ret;
+
+	ret = pdata->device_shutdown(pdev);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
+static int asvirt_rproc_probe(struct platform_device *pdev)
+{
+	const char* firmware_name;
+	struct device_node *np = pdev->dev.of_node;
+	struct asvirt_rproc *oproc;
+	struct rproc *rproc;
+	int ret;
+
+	ret = dma_set_coherent_mask(&pdev->dev, DMA_BIT_MASK(32));
+	if (ret) {
+		dev_err(&pdev->dev, "dma_set_coherent_mask: %d\n", ret);
+		return ret;
+	}
+
+	ret = of_property_read_string(np,"fwname",&firmware_name);
+	if (ret) {
+		dev_err(&pdev->dev, "of_property_read_string: %d\n", ret);
+		return ret;
+	}
+
+	rproc = rproc_alloc(&pdev->dev, firmware_name, &asvirt_rproc_ops,
+				NULL, sizeof(*oproc));
+	if (!rproc)
+		return -ENOMEM;
+
+	oproc = rproc->priv;
+	oproc->rproc = rproc;
+
+	platform_set_drvdata(pdev, rproc);
+
+	ret = rproc_add(rproc);
+	if (ret)
+		goto free_rproc;
+
+	dev_info(&pdev->dev, "initialized ASVIRT remote processor driver\n");
+
+	return 0;
+
+free_rproc:
+	rproc_put(rproc);
+	return ret;
+}
+
+static int asvirt_rproc_remove(struct platform_device *pdev)
+{
+	struct rproc *rproc = platform_get_drvdata(pdev);
+
+	rproc_del(rproc);
+	rproc_put(rproc);
+
+	return 0;
+}
+/* ============================ [ FUNCTIONS ] ====================================================== */
+
+
+module_platform_driver(asvirt_rproc_driver);
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("ASVIRT Remote Processor control driver");
