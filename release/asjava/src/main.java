8f5c3d0277881ad8b17074d3ee84c16119395540
parai.wang <parai@foxmail.com>
2017-01-30 20:58:43 +0800

    demo about class inherit and dynamic override

--- release/asjava/src/main.java
+++ release/asjava/src/main.java
@@ -15,6 +15,57 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
+class Father {
+	public Father() {
+		System.out.println("Constructor of Father!");
+	}
+
+	public String home() {
+		return "Zhongxiang, China";
+	}
+
+	public String firstName() {
+		return "Wang";
+	}
+
+	public String lastName() {
+		return "Shigui";
+	}
+
+	public String sex() {
+		return "male";
+	}
+
+	public String fullName() {
+		return firstName() + " " + lastName();
+	}
+}
+
+class Son extends Father {
+	private String last_name;
+
+	public Son(String last_name) {
+		System.out.println("Constructor of Son!");
+		this.last_name = last_name;
+	}
+
+	public String lastName() {
+		return last_name;
+	}
+}
+
+class Daughter extends Son {
+
+	public Daughter(String last_name) {
+		super(last_name);
+		System.out.println("Constructor of Daughter!");
+	}
+
+	public String sex() {
+		return "female";
+	}
+}
+
 class TimerEvent extends TimerTask {
 	private int counter = 0;
 
@@ -36,13 +87,25 @@ class TimerEvent extends TimerTask {
 public class main {
 
 	private static Timer timer;
-
-	public static void main(String[] argv) {
-
+	
+	private static void startTimer() {
 		timer = new Timer();	//creates a new timer to schedule
 		timer.schedule(new TimerEvent(), 400, 400);
+	}
+
+	public static void main(String[] argv) {
+		Father boy,girl;
 		
 		System.out.println("Hello World!");
+		
+		boy = new Son("Fan");
+		
+		System.out.println(boy.fullName() + " is born at " + boy.home() + " , is " + boy.sex());
+		
+		girl = new Daughter("Fake");
+		System.out.println(girl.fullName() + " is born at " + girl.home() + " , is " + girl.sex());
+		
+		startTimer();
 	}
 }
 
