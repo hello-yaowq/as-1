9a8c948820e4b34ac1f3ba435381cc6190684452
Parai Wang <parai@foxmail.com>
2017-04-10 14:30:40 +0800

    add ucos_ii for study purpose

---
+++ release/ucos_ii/demo/SystemConfig.h
@@ -0,0 +1,37 @@
+#ifndef SYSTEM_CONFIG_H
+#define SYSTEM_CONFIG_H
+
+#define _CRT_SECURE_NO_WARNINGS	
+
+#include <stdio.h>
+#include <assert.h>
+#include <includes.h>		
+/*
+*********************************************************************************************************
+*                                            TASK PRIORITIES
+*********************************************************************************************************
+*/
+
+#define MainTask_Prio 5
+#define App1Task_Prio 7
+#define App2Task_Prio 9
+
+/*
+*********************************************************************************************************
+*                                            TASK STACK SIZES
+*********************************************************************************************************
+*/
+
+#define MainTask_StkSize 1024
+#define App1Task_StkSize 1024
+#define App2Task_StkSize 1024
+#define OS_TASK_TMR_PRIO 65533
+#define OS_IDLE_PRIO OS_TASK_IDLE_PRIO
+#define OS_STAT_PRIO OS_TASK_STAT_PRIO
+
+
+extern void MainTask(void *p_arg);
+extern void App1Task(void *p_arg);
+extern void App2Task(void *p_arg);
+
+#endif
---
+++ release/ucos_ii/demo/main.c
@@ -0,0 +1,59 @@
+#include "SystemConfig.h"
+
+
+
+OS_STK MainTask_Stk[MainTask_StkSize];
+OS_STK App1Task_Stk[App1Task_StkSize];
+OS_STK App2Task_Stk[App2Task_StkSize];
+int main(void)
+{
+	OSInit();
+
+	OSTaskCreate(MainTask, (void *)0, &MainTask_Stk[MainTask_StkSize-1], MainTask_Prio);
+	OSStart();
+	return 0;
+}
+
+void MainTask(void *p_arg)
+{
+	p_arg = p_arg;
+	OSStatInit();
+	while(1)
+	{
+
+		OSTaskCreate(App1Task, (void *)0, &App1Task_Stk[App1Task_StkSize-1], App1Task_Prio);
+		OSTaskCreate(App2Task, (void *)0, &App2Task_Stk[App2Task_StkSize-1], App2Task_Prio);
+		printf("Hello,I am Main!\n");
+		OSTimeDlyHMSM(0, 1, 0, 0);
+		printf("Hello,Main End!\n");
+        OSTaskDel(MainTask_Prio);
+	}
+}
+/* App1Task */
+void App1Task(void *p_arg)
+{
+	p_arg = p_arg;
+	while(1)
+	{
+		printf("Hello,I am PC!\n");
+		OSTimeDlyHMSM(0, 0, 1, 0);
+	}
+}
+/* App2Task */
+void App2Task(void *p_arg)
+{
+	p_arg = p_arg;
+	while(1)
+	{
+		printf("Hello,uCOS-II!\n");
+		OSTimeDlyHMSM(0, 0, 2, 0);
+	}
+}
+
+void OSDebugInit()
+{
+}
+void OSTaskReturnHook(OS_TCB *ptcb)
+{
+
+}
