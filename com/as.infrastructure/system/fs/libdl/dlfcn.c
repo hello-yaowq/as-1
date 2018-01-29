a31e0d4ca47e7e34eb2d9b0f8d92546b4084ba70
parai <parai@foxmail.com>
2018-01-29 23:51:19 +0800

    init dlopen, not tested

--- com/as.infrastructure/system/fs/libdl/dlfcn.c
+++ com/as.infrastructure/system/fs/libdl/dlfcn.c
@@ -17,26 +17,81 @@
 #include "dlfcn.h"
 #include "vfs.h"
 #include "elf/elf.h"
+#ifdef USE_SHELL
+#include "shell.h"
+#endif
 /* ============================ [ MACROS    ] ====================================================== */
 /* ============================ [ TYPES     ] ====================================================== */
 typedef struct
 {
 	void* addr; /* module loaded in memory, the start address */
 }dll_t;
-/* ============================ [ DATAS     ] ====================================================== */
+
 /* ============================ [ DECLARES  ] ====================================================== */
+#ifdef USE_SHELL
+static int dllFunc(int argc, char* argv[]);
+#endif
+/* ============================ [ DATAS     ] ====================================================== */
+#ifdef USE_SHELL
+static SHELL_CONST ShellCmdT dllCmd  = {
+	dllFunc,
+	1,1,
+	"dll",
+	"dll [path]",
+	"load dll and run the code from the main entry\n",
+	{NULL,NULL}
+};
+SHELL_CMD_EXPORT(dllCmd);
+#endif
 /* ============================ [ LOCALS    ] ====================================================== */
+#ifdef USE_SHELL
+static int dllFunc(int argc, char* argv[])
+{
+	int r = 0;
+	dll_t* dll = dlopen(argv[1], RTLD_NOW);
+	if(NULL != dll)
+	{
+		
+	}
+	else
+	{
+		r = -1;
+	}
+	return r;
+}
+#endif
 /* ============================ [ FUNCTIONS ] ====================================================== */
 void *dlopen(const char *filename, int flag)
 {
 	dll_t* dll = NULL;
 	VFS_FILE* fp;
+	struct vfs_stat st;
+	(void) flag; /* always RTLD_NOW */
 
-	fp = vfs_fopen(filename, "rb");
-	
-	if(NULL != fp)
+	if( (0==vfs_stat(filename, &st)) &&
+		(VFS_ISREG(st.st_mode)) )
 	{
+		fp = vfs_fopen(filename, "rb");
+	
+		if(NULL != fp)
+		{
+			dll = malloc(st.st_size+sizeof(dll_t));
+			if(NULL != dll)
+			{
+				dll->addr = &dll[1];
+				vfs_fread(dll->addr, st.st_size, 1, fp);
+			}
+			vfs_fclose(fp);
+		}
+	}
 
+	if(NULL != dll)
+	{
+		if(1 != elf_loadFile(dll->addr, 0))
+		{
+			free(dll);
+			dll = NULL;
+		}
 	}
 
 	return dll;
