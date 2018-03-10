a623bfc7599a1a6fbca316abb3a9ba28ee5e6447
parai.wang <parai@foxmail.com>
2018-03-10 20:33:19 +0800

    init base of pbl

---
+++ com/as.infrastructure/boot/common/pbl_core.c
@@ -0,0 +1,27 @@
+/**
+ * AS - the open source Automotive Software on https://github.com/parai
+ *
+ * Copyright (C) 2018  AS <parai@foxmail.com>
+ *
+ * This source code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation; See <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+#ifdef USE_XCP
+/* ============================ [ INCLUDES  ] ====================================================== */
+#include "bootloader.h"
+/* ============================ [ MACROS    ] ====================================================== */
+
+/* ============================ [ TYPES     ] ====================================================== */
+/* ============================ [ DECLARES  ] ====================================================== */
+/* ============================ [ DATAS     ] ====================================================== */
+uint32_t g_PBLAddress;
+uint32_t g_PBLSize;
+/* ============================ [ LOCALS    ] ====================================================== */
+/* ============================ [ FUNCTIONS ] ====================================================== */
+#endif /* USE_XCP */
